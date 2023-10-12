ca09f42d7cfedfb7f428c2272372541c54f383da
Premchand Pawade <130446286+premchandpawade@users.noreply.github.com>
2023-10-12 23:24:12 +0530

    Add files via upload
    student grading system in cpp

---
+++ t3.cpp
@@ -0,0 +1,54 @@
+//****************************************student grading system******************************3
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Student {
+    string name;
+    double grade;
+};
+
+int main() {
+    vector<Student> students;
+    int numStudents;
+
+    cout << "Enter the total number of students: ";
+    cin >> numStudents;
+
+    for (int i = 0; i < numStudents; ++i) {
+        Student student;
+        cout << "Enter the name of student " << i + 1 << ": "<<endl;
+        cin >> student.name;
+        cout << "Enter the grade of student " << i + 1 << ": "<<endl;
+        cin >> student.grade;
+        students.push_back(student);
+    }
+
+    double sumGrades = 0.0;
+    for (const auto& student : students) {
+        sumGrades += student.grade;
+    }
+    double averageGrade = sumGrades / numStudents;
+
+    double highestGrade = students[0].grade;
+    double lowestGrade = students[0].grade;
+    for (const auto& student : students) {
+        highestGrade = max(highestGrade, student.grade);
+        lowestGrade = min(lowestGrade, student.grade);
+    }
+
+    cout << "\nStudent Grades Summary:\n";
+    for (const auto& student : students) {
+        cout << student.name << ": " << student.grade << endl;
+    }
+
+    cout << "\nAverage Grade: " << averageGrade << endl;
+    cout << "Highest Grade: " << highestGrade << endl;
+    cout << "Lowest Grade: " << lowestGrade << endl;
+
+    return 0;
+}
